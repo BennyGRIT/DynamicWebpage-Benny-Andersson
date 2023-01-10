7ca39d1e7726bbdf1bc26a726d6984997a8f69ad
Benny Andersson <hawkhbg@gmail.com>
2023-01-10 19:31:26 +0100

    Grid and mediaQuerys

---
+++ main.js
@@ -0,0 +1,7 @@
+const buyButton = document.querySelectorAll("buyButton");
+
+function myBuy(){
+    alert(`Du har nu köpt denna fina pjäs!! 
+    Var vänlig lägg tie TUSEN i omärkta sedlar i min brevlåda!
+    (Så fixar jag växel till dig senare)`)
+}
