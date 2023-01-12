66b3b5ce01c3b4d036d97200421c34097f2d13a8
Benny Andersson <hawkhbg@gmail.com>
2023-01-12 18:18:23 +0100

    Eng-Swe auto är klar

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-
-
-
 let darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
-console.log(darkMode?"Darkmode":"Lightmode");
 
-
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-const newColorScheme = event.matches ? "nowDarkMode" : "nowLightMode";
-// alert(newColorScheme)
-console.log(newColorScheme)
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (event) => {
+    const newColorScheme = event.matches ? "nowDarkMode" : "nowLightMode";
+    // alert(newColorScheme)
+    console.log(newColorScheme);
 
     // if (newColorScheme === "nowDarkMode"){
     //     console.log('if darkmode')
@@ -22,55 +19,106 @@ console.log(newColorScheme)
 
     // }
 
-    if (darkMode){
-        console.log('if darkmode')
-        let lightOrDarkMode  ="darkModeClass"
-        theBody.setAttribute('class', lightOrDarkMode);
-        window.localStorage.setItem("lightOrDarkMode", lightOrDarkMode)
+    if (darkMode) {
+      console.log("if darkmode");
+      let lightOrDarkMode = "darkModeClass";
+      theBody.setAttribute("class", lightOrDarkMode);
+      window.localStorage.setItem("lightOrDarkMode", lightOrDarkMode);
     }
-    if (darkMode = window.matchMedia("(prefers-color-scheme: light)").matches) {
-        let lightOrDarkMode  ="lightModeClass"
-        
-        console.log('else lightmode');
-        theBody.setAttribute('class', lightOrDarkMode);
-        window.localStorage.setItem("lightOrDarkMode", lightOrDarkMode)
+    if (
+      (darkMode = window.matchMedia("(prefers-color-scheme: light)").matches)
+    ) {
+      let lightOrDarkMode = "lightModeClass";
 
+      console.log("else lightmode");
+      theBody.setAttribute("class", lightOrDarkMode);
+      window.localStorage.setItem("lightOrDarkMode", lightOrDarkMode);
     }
 
+    let lightOrDarkMode = theBody.setAttribute;
+  });
 
-
-
-
-    let lightOrDarkMode = theBody.setAttribute
-});
-
-
-
-function myBuy(){
-    alert(`Du har nu köpt denna fina pjäs!! 
+function myBuy() {
+  alert(`Du har nu köpt denna fina pjäs!! 
     Var vänlig lägg tie TUSEN i omärkta sedlar i min brevlåda!
-    (Så fixar jag växel till dig senare)`)
-};
+    (Så fixar jag växel till dig senare)`);
+}
 
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  };
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 
-  const buyButtonProduct10Button = document.getElementById('buyButtonProduct10');
+const buyButtonProduct10Button = document.getElementById("buyButtonProduct10");
 
-  const product10Id = document.getElementById('product10');
+const product10Id = document.getElementById("product10");
 
-  buyButtonProduct10Button.addEventListener('click', ()=>{
-    product10Id.style.transform='rotate(-0deg'
-  })
-  
+buyButtonProduct10Button.addEventListener("click", () => {
+  product10Id.style.transform = "rotate(-0deg";
+});
+
+let lang = navigator.language || navigator.userLanguage;
+console.log(lang);
+
+if (lang === "sv-SE") {
+  console.log("Tjenare mannen IF");
+} else if (lang === "en-US") {
+  console.log("Hello motherTRUCKER!!! if else");
+
+//   const test1 = document.getElementById('product1text')
+//   test1.innerHTML= 'dra åt fan din jävel'
+
+  const textInEnglish = [
+    "The ultimate LEGO® Star Wars Millennium Falcon has landed! With 7,500 parts, Han Solos super-fast Corellian ship is packed with the coolest details and fun features.",
+    "Build and display the Galactic Empires iconic ship – the Devastator.",
+    "Inspire children and adults with this 75257 LEGO® Star Wars™ Millennium Falcon model.",
+    "Team up with Luke Skywalker on the battlefield of Hoth and challenge General Veers in the mighty AT-AT.",
+    "The droid has movable legs and comes with a clear, brick-built pole so it can float over the buildable snow scene.",
+    "This cool building model includes a buildable display stand with an information sign.",
+    "Kids aged 9 and up can recreate epic stories from Star Wars: The Bad Batch with this amazing LEGO® brick model of bounty hunter Cad Banes spaceship, The Justifier (75323).",
+    "Recreate the Battle of Utapau with this awesome LEGO® Star Wars™ AT-TE Walker (75337)",
+    "Imagine life as a galactic bounty hunter as you build and display a LEGO Star Wars™ Ultimate Collector Series version of The Razor Crest starship (75331).",
+    "Fans of The Empire Strikes Back can stage Imperial missions to defeat the Rebel Alliance with this TIE Bomber (75347) LEGO brick starfighter.",
+  ];
+
+  const product1 = document.getElementById("product1text");
+  const product2 = document.getElementById("product2text");
+  const product3 = document.getElementById("product3text");
+  const product4 = document.getElementById("product4text");
+  const product5 = document.getElementById("product5text");
+  const product6 = document.getElementById("product6text");
+  const product7 = document.getElementById("product7text");
+  const product8 = document.getElementById("product8text");
+  const product9 = document.getElementById("product9text");
+  const product10 = document.getElementById("product10text");
+
+  const allProducts = [product1, product2, product3, product4, product5,
+product6, product7, product8, product9, product10]
+
+// Benny fattar inte varför nedanstående loop inte fungerar. Han får snart ett damp-anfall
+//   for (let i = 0; i<allProducts.length; i++) {
+//     allProducts[i].innerHMTL=textInEnglish[i]
+// }
 
 
+allProducts[0].innerHTML=textInEnglish[0]
+allProducts[1].innerHTML=textInEnglish[1]
+allProducts[2].innerHTML=textInEnglish[2]
+allProducts[3].innerHTML=textInEnglish[3]
+allProducts[4].innerHTML=textInEnglish[4]
+allProducts[5].innerHTML=textInEnglish[5]
+allProducts[6].innerHTML=textInEnglish[6]
+allProducts[7].innerHTML=textInEnglish[7]
+allProducts[8].innerHTML=textInEnglish[8]
+allProducts[9].innerHTML=textInEnglish[9]
+
+
+
+}
 
 //     let darkMode = window.matchMedia("(prefers-color-scheme: ligth)").matches;
 //     console.log(darkMode?"Darkmode":"Lightmode");
@@ -81,11 +129,7 @@ function myFunction() {
 //     console.log(newColorScheme)
 //     });
 
-
-
-
 // const isItDarkOrLightButton = document.getElementById('isItDarkMode')
-
 
 //     isItDarkOrLightButton.addEventListener('click',event =>{
 //     console.log(darkMode?"Darkmode":"Lightmode");
@@ -97,7 +141,7 @@ function myFunction() {
 //     alert(newColorScheme)
 
 // })
-let myGetItem = window.localStorage.getItem("lightOrDarkMode")
-const theBody = document.querySelector('body')
+let myGetItem = window.localStorage.getItem("lightOrDarkMode");
+const theBody = document.querySelector("body");
 
-theBody.setAttribute('class', myGetItem);
+theBody.setAttribute("class", myGetItem);
